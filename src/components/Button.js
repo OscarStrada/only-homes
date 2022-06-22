c938320788dc5f389f1324ed5cfb37466d8d6c14
Oscar Estrada <arturostrada2020@gmail.com>
2022-06-22 04:18:58 -0500

    refactor button component for small size

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,16 @@
-import { ArrowRightIcon } from '@heroicons/react/solid';
-
 import React from 'react';
 
-export const Button = ({ children, color, rightArrow, textColor }) => {
+export const Button = ({ small, children, color, textcolor }) => {
   return (
     <div
-      className="flex items-center justify-center space-x-4 w-full h-[55px] rounded-md cursor-pointer select-none tracking-[0.2em] uppercase font-inter text-xs"
-      style={{ backgroundColor: `${color}`, color: `${textColor}` }}
+      className={`${
+        small
+          ? 'h-[30px] text-[10px] rounded-sm'
+          : 'h-[55px] text-xs rounded-md'
+      } flex items-center justify-center space-x-4 w-full cursor-pointer select-none tracking-[0.2em] uppercase font-inter`}
+      style={{ backgroundColor: `${color}`, color: `${textcolor}` }}
     >
       <div>{children}</div>
-      {rightArrow && <ArrowRightIcon className="w-[18px]" />}
     </div>
   );
 };

bc4720b86b54a665e6ad9293a2c2adc1796b3d75
Oscar Estrada <arturostrada2020@gmail.com>
2022-06-27 16:05:49 -0500

    delete pt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <div className="w-full px-[25px] md:px-20 mt-[200px] pb-10 flex justify-center md:justify-end items-center">
+    <div className="w-full px-[25px] md:px-20 pb-10 flex justify-center md:justify-end items-center">
       <div className="text-center md:text-right w-3/4 md:w-full">
         <span className="uppercase text-[#556277] text-sm tracking-widest">
           © 2022 Only homes Tulum,
